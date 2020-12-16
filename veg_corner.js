0bf63d437c35e2b2f3fa8d9a79756b338f0b0306
Anik Barua <anik.uits29@gmail.com>
2020-12-16 14:42:47 +0600

    Tatka's initial commit

---
+++ veg_corner.js
@@ -0,0 +1,421 @@
+var toggled1=false;
+
+function toggle1(){
+    if(!toggled1){
+        toggled1= true;
+        $("#btn1").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled1= false;
+        $("#btn1").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+var toggled2=false;
+
+function toggle2(){
+    if(!toggled2){
+        toggled2= true;
+        $("#btn2").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled2= false;
+        $("#btn2").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+var toggled3=false;
+
+function toggle3(){
+    if(!toggled3){
+        toggled3= true;
+        $("#btn3").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled3= false;
+        $("#btn3").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled4=false;
+
+function toggle4(){
+    if(!toggled4){
+        toggled4= true;
+        $("#btn4").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled4= false;
+        $("#btn4").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled5=false;
+
+function toggle5(){
+    if(!toggled5){
+        toggled5= true;
+        $("#btn5").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled5= false;
+        $("#btn5").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled6=false;
+
+function toggle6(){
+    if(!toggled6){
+        toggled6= true;
+        $("#btn6").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled6= false;
+        $("#btn6").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled7=false;
+
+function toggle7(){
+    if(!toggled7){
+        toggled7= true;
+        $("#btn7").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled7= false;
+        $("#btn7").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled8=false;
+
+function toggle8(){
+    if(!toggled8){
+        toggled8= true;
+        $("#btn8").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled8= false;
+        $("#btn8").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled9=false;
+
+function toggle9(){
+    if(!toggled9){
+        toggled9= true;
+        $("#btn9").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled9= false;
+        $("#btn9").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled10=false;
+
+function toggle10(){
+    if(!toggled10){
+        toggled10= true;
+        $("#btn10").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled10= false;
+        $("#btn10").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled11=false;
+
+function toggle11(){
+    if(!toggled11){
+        toggled11= true;
+        $("#btn11").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled11= false;
+        $("#btn11").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled12=false;
+
+function toggle12(){
+    if(!toggled12){
+        toggled12= true;
+        $("#btn12").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled12= false;
+        $("#btn12").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled13=false;
+
+function toggle13(){
+    if(!toggled13){
+        toggled13= true;
+        $("#btn13").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled13= false;
+        $("#btn13").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled14=false;
+
+function toggle14(){
+    if(!toggled14){
+        toggled14= true;
+        $("#btn14").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled14= false;
+        $("#btn14").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled15=false;
+
+function toggle15(){
+    if(!toggled15){
+        toggled15= true;
+        $("#btn15").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled15= false;
+        $("#btn15").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled16=false;
+
+function toggle16(){
+    if(!toggled16){
+        toggled16= true;
+        $("#btn16").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled16= false;
+        $("#btn16").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled17=false;
+
+function toggle17(){
+    if(!toggled17){
+        toggled17= true;
+        $("#btn17").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled17= false;
+        $("#btn17").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled18=false;
+
+function toggle18(){
+    if(!toggled18){
+        toggled18= true;
+        $("#btn18").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled18= false;
+        $("#btn18").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled19=false;
+
+function toggle19(){
+    if(!toggled19){
+        toggled19= true;
+        $("#btn19").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled19= false;
+        $("#btn19").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled20=false;
+
+function toggle20(){
+    if(!toggled20){
+        toggled20= true;
+        $("#btn20").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled20= false;
+        $("#btn20").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled21=false;
+
+function toggle21(){
+    if(!toggled21){
+        toggled21= true;
+        $("#btn21").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled21= false;
+        $("#btn21").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled22=false;
+
+function toggle22(){
+    if(!toggled22){
+        toggled22= true;
+        $("#btn22").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled22= false;
+        $("#btn22").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled23=false;
+
+function toggle23(){
+    if(!toggled23){
+        toggled23= true;
+        $("#btn23").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled23= false;
+        $("#btn23").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled24=false;
+
+function toggle24(){
+    if(!toggled24){
+        toggled24= true;
+        $("#btn24").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled24= false;
+        $("#btn24").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+var toggled25=false;
+
+function toggle25(){
+    if(!toggled25){
+        toggled25= true;
+        $("#btn25").addClass("cancel").html("Cancel");
+        return;
+    }
+    else{
+        toggled25= false;
+        $("#btn25").removeClass("cancel").html("ADD");
+        return;
+    }
+}
+
+
+
+var resultField= $("#result");
+
+function insertNumber(number){
+    var existingNumber= resultField.val()
+    resultField.val(existingNumber + number)
+}
+
+function clearResult(){
+    resultField.val('')
+}
+
+function calculate(){
+   var result=  eval(resultField.val())
+   resultField.val(result)
+}
+
+function deleteNumber(){
+    var presentValue= resultField.val();
+    if(presentValue!=''){
+        resultField.val(presentValue.slice(0,-1))
+    }
+ }
